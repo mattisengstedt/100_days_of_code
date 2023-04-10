7851f5feaba73241e78ddfe5ca3681e95976498c
mattisengstedt <mattis.engstedt@hotmail.com>
2023-04-10 22:08:29 +0000

    Första dagen på 100 days of code

---
+++ day_1/main.py
@@ -0,0 +1,5 @@
+#/n används för nya rader och funkar på input
+name_of_city = input('What city did you grow up in?\n')
+name_of_pet = input('\nWhat was the name of your first pet?\n')
+
+print('\nYour band name could be ' + name_of_city + ' ' + name_of_pet + '.')
